b326de7cf75fbdb96c52b99cc61c7be67df83316
Vu Nguyen <vubnguyen@gmail.com>
2015-09-27 20:35:11 -0700

    all requirements.

---
+++ Yelp/RadioCell.swift
@@ -0,0 +1,24 @@
+//
+//  RadioCell.swift
+//  Yelp
+//
+//  Created by vu on 9/27/15.
+//  Copyright © 2015 Timothy Lee. All rights reserved.
+//
+
+import UIKit
+
+class RadioCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
